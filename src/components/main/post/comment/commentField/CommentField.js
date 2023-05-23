10b4bb48ec9d1356f397e5be16f947550bee99db
Kirillbasenko <95930608+Kirillbasenko@users.noreply.github.com>
2023-05-23 12:08:57 +0300

    comment style 1.18

--- src/components/main/post/comment/commentField/CommentField.js
+++ src/components/main/post/comment/commentField/CommentField.js
@@ -109,7 +109,7 @@ const CommentField = ({setArr, focus, id, userPhoto, commentParent}) => {
                         multiline
                         maxRows={4}
                         sx={{
-                           //width: !commentParent ? 583 : 513,
+                           width: !commentParent ? 583 : 513,
                            borderRadius: "20px",
                            position: "relative",
                            zIndex: 1,
