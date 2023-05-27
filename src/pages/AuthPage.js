9c5c92711ac7500014940cd85afc01e12cbc1e54
Kirill <kirillbasenko6@gmail.com>
2023-05-27 21:56:14 +0300

    auth style 1.02

--- src/pages/AuthPage.js
+++ src/pages/AuthPage.js
@@ -16,7 +16,16 @@ const AuthPage = () => {
    const [position, setPosition] = useState(false)
 
    return(
-      <Card className={styles.parent} sx={{position: "relative"}}>
+      <Card 
+         className={styles.parent} 
+         sx={{
+            position: "relative",
+            marginTop: "8%",
+            minWidth: "500px",
+            minHeight: "700px",
+            backgroundColor: "#14204bad",
+            borderRadius: "12px"
+            }}>
          <Grid container className={styles.wrapper}>
             <Grid item xs={6}>
                <CardMedia
@@ -34,11 +43,44 @@ const AuthPage = () => {
                   alt="green iguana"/>
                   <Box sx={{
                            transition: "transform 1s",
-                           transform: position ? "translateX(100%)" : "translateX(0)"}}
+                           transform: position ? "translateX(100%)" : "translateX(0)",
+                           position: "relative",
+                           zIndex: 3,
+                           textAlign: "center",
+                           display: "flex",
+                           top: "550px",
+                           flexDirection: "column",
+                           justifyContent: "center"
+                        }}
                         className={styles.content}>
-                     <Typography variant='h4' className={styles.title}>Welcome to Pandora</Typography>
-                     <Typography className={styles.text}>{position ? "Already a user?" : "Need an account?"}</Typography>
-                     <Button onClick={() => setPosition(!position)} className={styles.button} variant="outlined">{position ? "sing in" : "sing up"}</Button>
+                     <Typography 
+                        variant='h4' 
+                        className={styles.title}
+                        sx={{
+                           color: "#386BE2",
+                           marginBottom: "15px"
+                        }}>
+                        Welcome to Pandora
+                     </Typography>
+                     <Typography 
+                        className={styles.text}
+                        sx={{
+                           color: "#386BE2"
+                        }}>
+                        {position ? "Already a user?" : "Need an account?"}
+                     </Typography>
+                     <Button 
+                        onClick={() => setPosition(!position)} 
+                        className={styles.button} 
+                        variant="outlined"
+                        sx={{
+                           margin: "0 auto",
+                           width: "170px",
+                           padding: "5px",
+                           fontSize: "12px"
+                        }}>
+                        {position ? "sing in" : "sing up"}
+                     </Button>
                   </Box>
             </Grid>
             <Grid item xs={6}>
